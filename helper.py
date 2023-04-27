890f024d93994f2727fc942b04c772eae7bddf8a
PierXuY <98803367+PierXuY@users.noreply.github.com>
2023-04-27 14:53:11 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -71,7 +71,6 @@ def show_each_message(message: str, role: str, area=None):
         background_color = gpt_background_color
     # \:是streamlit中的转义语法
     message = (url_correction(message)
-               .replace('\n', '\n\n')
                .replace(':', '\:'))
     area[0](f"\n<div class='avatar'>{icon}<h2>{name}：</h2></div>", unsafe_allow_html=True)
     area[1](f"""<div class='content-div' style='background-color: {background_color};'>\n\n{message}""",
