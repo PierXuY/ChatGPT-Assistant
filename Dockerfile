1e9e61866a695751e7e1c63489c58ad3e1b6a4f3
fastjrun <fastjrun@139.com>
2023-09-11 21:23:54 +0800

    容器中只有设置了环境变量OPENAI_API_KEY，才能在secrets.toml设置apikey；只有设置了环境变量OPENAI_API_BASE，才会更新secrets.toml的api_base

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ WORKDIR /opt/chatgpt-ai
 
 ADD ./ ./
 
-ENV OPENAI_API_KEY=sk-XXXXXXXXX
-ENV OPENAI_API_BASE=https://openaiproxy.xxx.com/v1
+# ENV OPENAI_API_KEY=sk-XXXXXXXXX
+# ENV OPENAI_API_BASE=https://api.openai.com/v1
 
 RUN pip install --no-cache-dir --upgrade -r requirements.txt --timeout 1000
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
-sed -i "1i\apikey = \"$OPENAI_API_KEY\"" ./.streamlit/secrets.toml &
-sed -i "s#^api_base.*#api_base = \"$OPENAI_API_BASE\"#" ./.streamlit/secrets.toml &
+if [ -n "$OPENAI_API_KEY" ]; then
+    sed -i "1i\apikey = \"$OPENAI_API_KEY\"" ./.streamlit/secrets.toml
+fi
+if [ -n "$OPENAI_API_BASE" ]; then
+    sed -i "s#^api_base.*#api_base = \"$OPENAI_API_BASE\"#" ./.streamlit/secrets.toml
+fi
 streamlit run app.py --server.port=8501 --server.enableCORS false --server.enableXsrfProtection false
-
-sed -i "s#^apiBase.*#api_base = \"$OPENAI_API_BASE\"#" ./.streamlit/secrets.toml
