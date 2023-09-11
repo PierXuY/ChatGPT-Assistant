71830ab8828827c072c283b8ee531472b866225d
fastjrun <fastjrun@139.com>
2023-09-11 21:54:15 +0800

    优化启动脚本，修复每次启动都会添加apikey的bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 if [ -n "$OPENAI_API_KEY" ]; then
-    sed -i "1i\apikey = \"$OPENAI_API_KEY\"" ./.streamlit/secrets.toml
+    if grep -q "apikey" "./.streamlit/secrets.toml"; then
+        sed -i "s#^apikey.*#apikey = \"$OPENAI_API_KEY\"#" ./.streamlit/secrets.toml
+    else
+        sed -i "1i\apikey = \"$OPENAI_API_KEY\"" ./.streamlit/secrets.toml
+    fi
 fi
 if [ -n "$OPENAI_API_BASE" ]; then
+    if grep -q "api_base" "./.streamlit/secrets.toml"; then
     sed -i "s#^api_base.*#api_base = \"$OPENAI_API_BASE\"#" ./.streamlit/secrets.toml
+    else
+        sed -i "1i\api_base = \"$OPENAI_API_BASE\"" ./.streamlit/secrets.toml
+    fi
 fi
 streamlit run app.py --server.port=8501 --server.enableCORS false --server.enableXsrfProtection false
